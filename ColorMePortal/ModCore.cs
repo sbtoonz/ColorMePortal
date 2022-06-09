73e8eeecf625a89d2eac52ea2a6ec7f87904f5b1
sbtoonz <sbtoonz@gmail.com>
2022-06-09 12:56:13 -0600

    It works

--- ColorMePortal/ModCore.cs
+++ ColorMePortal/ModCore.cs
@@ -29,7 +29,7 @@ namespace ColorMePortal
         private static ConfigEntry<Color> GradientColor2 = null!;
         private static ConfigEntry<float> GradientAlpha1 = null!;
         private static ConfigEntry<float> GradientAlpha2 = null!;
-        //private static ConfigEntry<LogLevel> debuglevel = null!;
+        private static List<GameObject> existingPortals = new List<GameObject>();
 
         private static Harmony harmony = null!;
         ConfigSync configSync = new(ModGUID) 
@@ -48,14 +48,13 @@ namespace ColorMePortal
         public void Awake()
         {
             Assembly assembly = Assembly.GetExecutingAssembly();
-            harmony = new(ModGUID);
+            harmony = new Harmony(ModGUID);
             harmony.PatchAll(assembly);
             ServerConfigLocked = config("1 - General", "Lock Configuration", true, "If on, the configuration is locked and can be changed by server admins only.");
             configSync.AddLockingConfigEntry(ServerConfigLocked);
-            PortalLogger = base.Logger;
+            PortalLogger = Logger;
             SetupWatcher();
 
-            //ParticleGradient = config("General", "Particle System Gradient", new Gradient(), "Particle System Gradient");
             PointLightColor = config("General", "Point Light Color", new Color(0, 0, 0), "Point Light color");
             StartColor1 = config("General", "Start Particle Life Color 1", new Color(0, 0, 0), "Particle start Color 1");
             StartColor2 = config("General", "Start Particle Life Color 2", new Color(0, 0, 0), "Particle start Color 2");
@@ -63,12 +62,11 @@ namespace ColorMePortal
             GradientColor2 = config("General", "Gradient Color 2", new Color(0, 0, 0), "Gradient Color 2");
             GradientAlpha1 = config("General", "Gradient Alpha 1", 1f, "Gradient Alpha 1");
             GradientAlpha2 = config("General", "Gradient Alpha 2", 1f, "Gradient Alpha 2");
-            //debuglevel = config("General", "Debug Level", LogLevel.All, "Set the debug level");
         }
         
         private void SetupWatcher()
         {
-            FileSystemWatcher watcher = new(Paths.ConfigPath, ConfigFileName);
+            FileSystemWatcher watcher = new FileSystemWatcher(Paths.ConfigPath, ConfigFileName);
             watcher.Changed += ReadConfigValues;
             watcher.Created += ReadConfigValues;
             watcher.Renamed += ReadConfigValues;
@@ -101,30 +99,35 @@ namespace ColorMePortal
         private static void AlterPortalColor(GameObject portalFab, Gradient newColors, Color pointLightColor, Color startColor1, Color startColor2)
         {
             var pointlight = portalFab.transform.Find("_target_found_red/Point light").gameObject.GetComponent<Light>();
-            ParticleSystem suckParticleSystem = portalFab.transform.Find("_target_found_red/suck particles").gameObject.GetComponent<ParticleSystem>();
-            ParticleSystem blueFlames = portalFab.transform.Find("_target_found_red/blue flames").gameObject.GetComponent<ParticleSystem>();
-            ParticleSystem wispParticles = portalFab.transform.Find("_target_found_red/Particle System").gameObject.GetComponent<ParticleSystem>();
-            Material FlameMat = blueFlames.gameObject.GetComponent<Renderer>().material;
+            var suckParticleSystem = portalFab.transform.Find("_target_found_red/suck particles").gameObject.GetComponent<ParticleSystem>();
+            var blueFlames = portalFab.transform.Find("_target_found_red/blue flames").gameObject.GetComponent<ParticleSystem>();
+            var wispParticles = portalFab.transform.Find("_target_found_red/Particle System").gameObject.GetComponent<ParticleSystem>();
+            var flameMat = blueFlames.gameObject.GetComponent<Renderer>().material;
             
             var suckcolor = suckParticleSystem.colorOverLifetime;
             suckcolor.enabled = true;
             Gradient grad = newColors;
             suckcolor.color = grad;
             var main = suckParticleSystem.main;
+            // ReSharper disable once NotAccessedVariable
             var suckcolor1 = main.startColor.colorMax;
             suckcolor1= startColor1;
+            // ReSharper disable once NotAccessedVariable
             var suckcolor2 = main.startColor.colorMin;
             suckcolor2= startColor2;
             
             
-            var FlameColor = blueFlames.colorOverLifetime;
-            FlameColor.enabled = true;
-            Gradient flameColorColor = newColors;
-            FlameColor.color = flameColorColor;
-            FlameMat.SetColor(Color1, startColor1);
+            var flameColor = blueFlames.colorOverLifetime;
+            flameColor.enabled = true;
+            flameColor.color = newColors;
+            flameMat.SetColor(Color1, startColor1);
+
+
             var blueFlamesmain = blueFlames.main;
+            // ReSharper disable once NotAccessedVariable
             var blueFlamessuckcolor1 = blueFlamesmain.startColor.colorMax;
             blueFlamessuckcolor1= startColor1;
+            // ReSharper disable once NotAccessedVariable
             var blueFlamessuckcolor2 = blueFlamesmain.startColor.colorMin;
             blueFlamessuckcolor2= startColor2;
             
@@ -133,22 +136,23 @@ namespace ColorMePortal
             Gradient wispcolorColor = newColors;
             wispcolor.color = wispcolorColor;
             var wispParticlesMain = wispParticles.main;
+            // ReSharper disable once NotAccessedVariable
             var wispParticlesColorMax = wispParticlesMain.startColor.colorMax;
             wispParticlesColorMax= startColor1;
+            // ReSharper disable once NotAccessedVariable
             var wispParticlesColorMin = wispParticlesMain.startColor.colorMin;
             wispParticlesColorMin= startColor2;
-
-
             pointlight.color = pointLightColor;
+
 
         }
 
         private static void ChangePrefabColors(object sender, FileSystemEventArgs e)
         {
             if(ZNetScene.instance == null)return;
-            GameObject portal = ZNetScene.instance.GetPrefab("portal_wood");
+            var portal = ZNetScene.instance.GetPrefab("portal_wood");
             PortalLogger.Log(LogLevel.Debug,"Found " + portal.name);
-            Gradient temp = new Gradient();
+            var temp = new Gradient();
             temp.SetKeys( new[]
             {
                 new GradientColorKey(GradientColor1.Value, 0.0f), 
@@ -160,6 +164,10 @@ namespace ColorMePortal
             } );
             
             AlterPortalColor(portal, temp, PointLightColor.Value, StartColor1.Value, StartColor2.Value);
+            foreach (var variable in existingPortals)
+            {
+                AlterPortalColor(variable, temp, PointLightColor.Value, StartColor1.Value, StartColor2.Value);
+            }
             PortalLogger.Log(LogLevel.Debug,"Changed Colors");
             ZNetScene.instance.m_prefabs.Remove(portal);
             PortalLogger.Log(LogLevel.Debug,"Removing Old Portal");
@@ -167,11 +175,11 @@ namespace ColorMePortal
             PortalLogger.Log(LogLevel.Debug, "Inserting Colored Portal");
             List<ZDO> portalList = new();
             ZDOMan.instance.GetAllZDOsWithPrefab("portal_wood",portalList);
-            foreach (var VARIABLE in portalList)
+            foreach (var variable in portalList)
             {
-                VARIABLE.SetPrefab(portal.name.GetStableHashCode());
-                VARIABLE.m_prefab = ZNetScene.instance.GetPrefab(portal.name).name.GetStableHashCode();
-                VARIABLE.m_tempHaveRevision = true;
+                variable.SetPrefab(portal.name.GetStableHashCode());
+                variable.m_prefab = ZNetScene.instance.GetPrefab(portal.name).name.GetStableHashCode();
+                variable.m_tempHaveRevision = true;
             }
             PortalLogger.Log(LogLevel.Debug, "Done Setting ZDOs");
 
@@ -198,6 +206,7 @@ namespace ColorMePortal
                     } );
             
                     AlterPortalColor(__instance.gameObject, temp, PointLightColor.Value, StartColor1.Value, StartColor2.Value);
+                    existingPortals.Add(__instance.gameObject);
                 }
             }
         }
